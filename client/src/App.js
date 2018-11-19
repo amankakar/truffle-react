e037782eafac7ef98473e9c21d80986fabeabb64
Aman Khan <amankhan1017@gmail.com>
2018-11-20 02:09:49 +0500

    with out network config

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,24 @@ import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./utils/getWeb3";
 import truffleContract from "truffle-contract";
+import ipfs from "./ipfs";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  constructor(props) {
+    super(props);
+    this.captureFile = this.captureFile.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  state = {
+    ipfsHash: "",
+    storageValue: 0,
+    web3: null,
+    accounts: null,
+    instance: null,
+    buffer: null
+  };
 
   componentDidMount = async () => {
     try {
@@ -23,7 +36,8 @@ class App extends Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      console.log(instance);
+      this.setState({ web3, accounts, instance });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -33,18 +47,48 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.set(5, { from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.get();
-
-    // Update state with the result.
-    this.setState({ storageValue: response.toNumber() });
+  onSubmit = event => {
+    const { accounts, instance } = this.state;
+    event.preventDefault();
+    ipfs.files.add(this.state.buffer, (error, result) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+      instance
+        .set(result[0].hash, { from: this.state.accounts[0], gas: "1000000" })
+        .then(r => {
+          return instance.get.call(this.state.accounts[0]);
+        })
+        .then(ipfsHash => {
+          this.setState({ ipfsHash: result[0].hash });
+          console.log("ipfsHash:", this.state.ipfsHash);
+        });
+    });
   };
+  captureFile = event => {
+    event.preventDefault();
+    const file = event.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () => {
+      this.setState({ buffer: Buffer(reader.result) });
+      console.log("buffer:", this.state.buffer);
+    };
+  };
+
+  // runExample = async () => {
+  //   const { accounts, contract } = this.state;
+  //
+  //   // Stores a given value, 5 by default.
+  //   //  await contract.set(5, { from: accounts[0] });
+  //
+  //   // Get the value from the contract to prove it worked.
+  //   const ipfsHash = await contract.get();
+  //
+  //   // Update state with the result.
+  //   //this.setState({ ipfsHash: ipfsHash });
+  // };
 
   render() {
     if (!this.state.web3) {
@@ -52,17 +96,18 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 37</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1>upload images to IPFS & Ethereum</h1>
+        <h3>Your Image</h3>
+        <img
+          width="40%"
+          height="50%"
+          src={`https://ipfs.io/ipfs/${this.state.ipfsHash}`}
+          alt=""
+        />
+        <form onSubmit={this.onSubmit}>
+          <input type="file" onChange={this.captureFile} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
